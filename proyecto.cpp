ed49396efdca397419d93d5f3c4dc3a40f4dbfe0
yolandamartre <yolandamartre@hotmail.com>
2020-12-04 11:48:22 -0600

    Update proyecto.cpp

--- proyecto.cpp
+++ proyecto.cpp
@@ -4,7 +4,11 @@ using namespace std;
 
 int main() {
 
+  int variable1 = 10;
+  
   cout << "Hola a todos de parte de Yolanda" << endl;
+  
+  cout << "La variable 1 = " << variable1 << endl;
   
   return 0;
 }
