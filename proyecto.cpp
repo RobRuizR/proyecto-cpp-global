191b5dd2afd39d7430bc561202b602b34294c0e5
robruizr <rroberto.rruiz@gmail.com>
2020-12-04 11:49:53 -0600

    merge conflict forzado

--- proyecto.cpp
+++ proyecto.cpp
@@ -2,9 +2,17 @@
 #include <iostream>
 using namespace std;
 
+void func() {
+  cout << "hola!" << endl;
+}
+
 int main() {
+
+  int var = 300;
 
   cout << "Hola a todos de parte de Yolanda" << endl;
   
+  cout << "La variable"<< endl;
+
   return 0;
 }
